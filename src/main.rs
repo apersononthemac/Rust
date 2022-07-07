56695d6797ee607f40d53d5027aacd8ac1b299cd
apersononthemac <jakecapurton@gmail.com>
2022-07-07 14:46:06 +0000

    tuple

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,5 @@
-fn main() {
-    // addition
-    let sum = 5 + 10;
-
-    // subtraction
-    let difference = 95.5 - 4.3;
-
-    // multiplication
-    let product = 4 * 30;
-
-    // division
-    let quotient = 56.7 / 32.2;
-    let floored = 2 / 3; // Results in 0
-    let modulo = 14 % 4; // 5 MOD 6
-
-    // remainder
-    let remainder = 43 % 5;
-
-    println!("{}, {}, {}, {},\nmodulo is {},\n{}, {}", sum, difference, product, quotient, modulo, floored, remainder);
+fn main(){
+    //tuple data type
+    let x: (i64, bool, u32) = (-12, true, 16);
+    println!("{}", x.0);
 }
