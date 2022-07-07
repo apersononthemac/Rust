787df028dc6dc18953d72335b5f843f4b897a0d7
apersononthemac <jakecapurton@gmail.com>
2022-07-07 14:22:42 +0000

    arithmetic stuff

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 fn main() {
-    let x = 5;
+    // addition
+    let sum = 5 + 10;
 
+    // subtraction
+    let difference = 95.5 - 4.3;
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
+    // multiplication
+    let product = 4 * 30;
 
-    println!("The value of x is: {}", x);
+    // division
+    let quotient = 56.7 / 32.2;
+    let floored = 2 / 3; // Results in 0
+    let modulo = 14 % 4; // 5 MOD 6
+
+    // remainder
+    let remainder = 43 % 5;
+
+    println!("{}, {}, {}, {},\nmodulo is {},\n{}, {}", sum, difference, product, quotient, modulo, floored, remainder);
 }
